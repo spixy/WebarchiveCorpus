595f0d98b43710e5c3d82cd3a0523d427b7ffb2e
spixy <spixy16@gmail.com>
2019-04-27 00:16:13 +0200

    bugfix, tags on seperate lines

--- WebArchivCorpus.py
+++ WebArchivCorpus.py
@@ -3,19 +3,23 @@ import justext
 import requests
 from datetime import date
 
-def getToday():
-    return date.today().strftime("%y%m%d") + "000000"
-
 def printHelp():
     print("Usage: " + sys.argv[0] + " [web url] [date] [language]")
     print("  date        in 'yymmddhhmmss' format, default value is now (" + getToday() + ")")
     print("  language    default value is 'Czech'")
     exit()
 
+def getToday():
+    return date.today().strftime("%y%m%d") + "000000"
+
+def downloadWebsite(webUrl, dateTime):
+    waybackUrl = "https://wayback.webarchiv.cz/wayback/" + dateTime + "/" + webUrl
+    return requests.get(waybackUrl).text.encode('utf-8')
+
 def html_escape(text):
     return text.replace('<', '&lt;').replace('>', '&gt;')
 
-def writeToOutput(paragraphs, nooilerplate):
+def writeToOutput(paragraphs, no_boilerplate):
     for paragraph in paragraphs:
         if paragraph['class'] != 'bad':
             if paragraph['heading']:
@@ -28,17 +32,17 @@ def writeToOutput(paragraphs, nooilerplate):
             else:
                 tag = 'b'
         escaped_paragraph = html_escape(paragraph['text']).strip()
-        print(u'<%s> %s' % (tag, escaped_paragraph))
+        print('<' + tag + '>')
+        print(escaped_paragraph)
 
 if len(sys.argv) == 1:
     printHelp()
 
 webUrl = sys.argv[1]
-date = sys.argv[2] if len(sys.argv) >= 3 else getToday()
+dateTime = sys.argv[2] if len(sys.argv) >= 3 else getToday()
 language = sys.argv[3] if len(sys.argv) >= 4 else "Czech"
 
-waybackUrl = "https://wayback.webarchiv.cz/wayback/" + date + "/" + webUrl
-htmlContent = requests.get(waybackUrl).text.encode('utf-8')
+htmlContent = downloadWebsite(webUrl, dateTime)
 
 paragraphs = justext.justext(htmlContent, justext.get_stoplist(language))
 
